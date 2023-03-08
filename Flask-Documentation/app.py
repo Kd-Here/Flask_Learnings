44c669ca21b51f156e30534550e08cbd9250f534
Kd-Here <gamelover9899@gmail.com>
2023-03-08 20:36:20 +0530

    Accessing Request Data

--- Flask-Documentation/app.py
+++ Flask-Documentation/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
-@app.route('/hello')
+@app.route('/hello/')
 def hello():
     return 'Hello, World'
 
@@ -27,5 +27,62 @@ def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {(subpath)}'
 
-# Continue from here
-# https://flask.palletsprojects.com/en/2.2.x/quickstart/#unique-urls-redirection-behavior
+
+# Unique Urls behaviour
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return '<b>The about page</b>'
+
+# ###################
+#URL Building :- To build url from knowing the function is also called reversing.
+# ###################
+
+from flask import url_for
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('hello'))
+    print(url_for('hello', next='/'))
+    print(url_for('show_user_profile', username='kd'))
+    print(url_for('about'))
+    print(url_for('show_post',post_id=9899))
+    print(url_for('static', filename='style.css'))
+
+
+
+
+
+
+######################
+# Http requests 
+######################
+from flask import request
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "<h1>Accesing via post</h1>"
+    else:
+        return "<h1>Accesing via Get</h1>"
+    
+
+
+######################
+# Html page rendering
+######################
+
+from flask import render_template
+
+@app.route('/Home')
+def Home():
+    return render_template('home.html')
+
+
+
+###############
+#Accessing Request Data
+# https://flask.palletsprojects.com/en/2.2.x/quickstart/#accessing-request-data
+###############
